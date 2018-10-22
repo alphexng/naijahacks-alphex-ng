c9331c79cd53ba1e7428380a87aefbe20dfafb8b
daniellamarr <danielchidiebele@gmail.com>
2018-10-22 10:05:18 +0100

    Add CORS

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import Route from './routes/route';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 Route(app);
 
